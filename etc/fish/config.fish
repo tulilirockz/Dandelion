b8af5160623fad17d0c4f8e2b8496289c6571d3c
Tulili <tulilirockz.pub+git@gmail.com>
2023-04-11 00:54:54 -0300

    feat: alias to container's justfile and fix fisher install

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -4,7 +4,8 @@ if status is-interactive
 	alias grep "rg"
 	alias cat "bat"
 	alias history "atuin history list"
-	
+	alias container-just "just -f /etc/justfile"
+
 	starship init fish | source
 	atuin init fish | source
 end
